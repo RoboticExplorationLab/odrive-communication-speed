efe5481962ab18a336794612ce482511a337ecd7
Aaron Schultz <aaron@hschultz.net>
2019-11-03 21:04:39 -0800

    Test now uses second odrive

--- src/c_sdk_test.cpp
+++ src/c_sdk_test.cpp
@@ -15,9 +15,11 @@ int main(int argc, const char **argv) {
 
     ThreadPool_t tp_ptr = ThreadPool();
 
-    const char *odrive_serial_number = "35722173822280";
-    ODrive_t odrive_ptr = ODrive(odrive_serial_number);
-    int result = initODrive(odrive_ptr);
+    const char *odrv0_serial_number = "35722173822280";
+    const char *odrv1_serial_number = "60894957285687";
+    ODrive_t odrv0 = ODrive(odrv0_serial_number);
+    ODrive_t odrv1 = ODrive(odrv1_serial_number);
+    int result = initODrive(odrv0) & initODrive(odrv1);
     std::cout << "Result: " << result << std::endl;
 
     if(result != 0) {
@@ -26,21 +28,26 @@ int main(int argc, const char **argv) {
     }
 
     if(argc > 1 && argv[1][0] == 'c') {
-        runCalibration(odrive_ptr);
+        runCalibration(odrv0);
+        runCalibration(odrv1);
         do {
             std::cout << '\n' << "Press any key when calibration is done...";
         } while (std::cin.get() != '\n');
     }
 
-    allIdle(odrive_ptr);
-    result = allReady(odrive_ptr);
+    allIdle(odrv0);
+    allIdle(odrv1);
+    result = allReady(odrv0) & allReady(odrv1);
     std::cout << "Result: " << result << std::endl;
-    setCurrentCtrlMode(odrive_ptr);
-    float pos0, vel0, pos1, vel1;
+    setCurrentCtrlMode(odrv0);
+    setCurrentCtrlMode(odrv1);
+    float pos0_0, vel0_0, pos1_0, vel1_0;
+    float pos0_1, vel0_1, pos1_1, vel1_1;
     float cmd0 = 0.0;
     float cmd1 = 0.0;
 
-    addODriveToThreadPool(tp_ptr, odrive_ptr);
+    addODriveToThreadPool(tp_ptr, odrv0);
+    addODriveToThreadPool(tp_ptr, odrv1);
 
     std::cout << "Starting timing tests..." << std::endl;
     int NUM_LOOPS = 10000;
@@ -49,7 +56,8 @@ int main(int argc, const char **argv) {
     float mid_sum = 0;
     for (int i = 0; i < NUM_LOOPS; i++) {
         std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-        controlODrive(tp_ptr, odrive_ptr, cmd0, cmd1, &pos0, &vel0, &pos1, &vel1);
+        controlODrive(tp_ptr, odrv0, cmd0, cmd1, &pos0_0, &vel0_0, &pos1_0, &vel1_0);
+        controlODrive(tp_ptr, odrv1, cmd0, cmd1, &pos0_1, &vel0_1, &pos1_1, &vel1_1);
 
         std::chrono::steady_clock::time_point mid = std::chrono::steady_clock::now();
 
