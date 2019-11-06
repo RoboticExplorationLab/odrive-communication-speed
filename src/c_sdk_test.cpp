8a28b7417258026d98ae2020ffc1535b962dec81
Nathan Kau <nathankau@gmail.com>
2019-11-06 23:29:46 +0000

    Test for three odrives at a time

--- src/c_sdk_test.cpp
+++ src/c_sdk_test.cpp
@@ -19,20 +19,26 @@ int main(int argc, const char **argv) {
 
     const char *odrv0_serial_number = "35722173822280";
     const char *odrv1_serial_number = "60894957285687";
+    const char *odrv2_serial_number = "35619062370891";
     ODrive_t odrv0 = ODrive(odrv0_serial_number);
     std::cout << "Connected odrive0" << std::endl;
     ODrive_t odrv1 = ODrive(odrv1_serial_number);
     std::cout << "Connected odrive1" << std::endl;
+    ODrive_t odrv2 = ODrive(odrv2_serial_number);
+    std::cout << "Connected odrive2" << std::endl;
     
-    usleep(5000000);
 
     int result = initODrive(odrv0);
     std::cout << "Result from initializing odrv0: " << result << std::endl;    
     
-    usleep(5000000);
 
     result = initODrive(odrv1);
     std::cout << "Result from initializing odrv1: " << result << std::endl;
+
+    usleep(1000000);
+
+    int result2 = initODrive(odrv2);
+    std::cout << "Result from initializing odrv2: " << result2 << std::endl;
 
     if(result != 0) {
         std::cerr << "ERROR: Could not initialize ODrives" << std::endl;
@@ -42,6 +48,7 @@ int main(int argc, const char **argv) {
     if(argc > 1 && argv[1][0] == 'c') {
         runCalibration(odrv0);
         runCalibration(odrv1);
+        runCalibration(odrv2);
         do {
             std::cout << '\n' << "Press any key when calibration is done...";
         } while (std::cin.get() != '\n');
@@ -49,27 +56,32 @@ int main(int argc, const char **argv) {
 
     allIdle(odrv0);
     allIdle(odrv1);
-    result = allReady(odrv0) & allReady(odrv1);
-    std::cout << "Result: " << result << std::endl;
+    allIdle(odrv2);
+    result = allReady(odrv0) | allReady(odrv1) | allReady(odrv2);
+    std::cout << "Odrives ready: " << result << std::endl;
     setCurrentCtrlMode(odrv0);
     setCurrentCtrlMode(odrv1);
+    setCurrentCtrlMode(odrv2);
     float pos0_0, vel0_0, pos1_0, vel1_0;
     float pos0_1, vel0_1, pos1_1, vel1_1;
+    float pos0_2, vel0_2, pos1_2, vel1_2;
     float cmd0 = 0.0;
     float cmd1 = 0.0;
 
     addODriveToThreadPool(tp_ptr, odrv0);
     addODriveToThreadPool(tp_ptr, odrv1);
+    addODriveToThreadPool(tp_ptr, odrv2);
 
     std::cout << "Starting timing tests..." << std::endl;
     int NUM_LOOPS = 10000;
 
+    std::cout << "Single odrive test..." << std::endl;
     float total_sum = 0;
     float mid_sum = 0;
+
     for (int i = 0; i < NUM_LOOPS; i++) {
         std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
         controlODrive(tp_ptr, odrv0, cmd0, cmd1, &pos0_0, &vel0_0, &pos1_0, &vel1_0);
-        controlODrive(tp_ptr, odrv1, cmd0, cmd1, &pos0_1, &vel0_1, &pos1_1, &vel1_1);
 
         std::chrono::steady_clock::time_point mid = std::chrono::steady_clock::now();
 
@@ -83,6 +95,29 @@ int main(int argc, const char **argv) {
     float mid_avg = mid_sum / NUM_LOOPS;
     std::cout << "Average time for command = " << total_avg << std::endl;
     std::cout << "Average time sending commands = " << mid_avg << std::endl;
+
+    std::cout << "Triple odrive test..." << std::endl;
+    total_sum = 0;
+    mid_sum = 0;
+      
+    for (int i = 0; i < NUM_LOOPS; i++) {
+        std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+        controlODrive(tp_ptr, odrv0, cmd0, cmd1, &pos0_0, &vel0_0, &pos1_0, &vel1_0);
+        controlODrive(tp_ptr, odrv1, cmd0, cmd1, &pos0_1, &vel0_1, &pos1_1, &vel1_1);
+        controlODrive(tp_ptr, odrv2, cmd0, cmd1, &pos0_2, &vel0_2, &pos1_2, &vel1_2);
+
+        std::chrono::steady_clock::time_point mid = std::chrono::steady_clock::now();
+       
+        waitForThreads(tp_ptr);
+         
+         std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+         total_sum += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+         mid_sum += std::chrono::duration_cast<std::chrono::microseconds>(mid - start).count();
+     }   
+     total_avg = total_sum / NUM_LOOPS;
+     mid_avg = mid_sum / NUM_LOOPS;
+     std::cout << "Average time for command = " << total_avg << std::endl;
+     std::cout << "Average time sending commands = " << mid_avg << std::endl;
     
     return 1;
 }
